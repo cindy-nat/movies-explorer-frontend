ca28a44c4f4b1649b74067155607c19e8b6a624c
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-19 10:22:22 +0300

    added main component to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Main from "../Main/Main";
+// import Header from "../Header/Header";
 
 function App() {
   return (
-    <div className="App">
 
-    </div>
+<Switch>
+  <Route exact path = "/">
+    <Main/>
+  </Route>
+  <div className="page">
+    {/*<Header/>*/}
+  </div>
+</Switch>
+
   );
 }
 
