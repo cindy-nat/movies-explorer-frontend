fabbdc5949b43c30620f54ce78014a5a922ff1f1
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-19 10:19:18 +0300

    added header to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,13 +2,18 @@ import './Main.css';
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
 import AboutProject from "../AboutProject/AboutProject";
+import Header from "../Header/Header";
 
 function Main() {
   return (
     <>
-    <Promo/>
-    <NavTab/>
-    <AboutProject/>
+    <Header/>
+
+    <main className="main-page">
+      <Promo/>
+      <NavTab/>
+      <AboutProject/>
+    </main>
     </>
   );
 }
