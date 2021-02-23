7c45c469ba8e74a14161489ee48bf737271f7faf
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-23 16:00:09 +0300

    added layout for whole page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Main from "../Main/Main";
-// import Header from "../Header/Header";
 
 function App() {
   return (
-
-<Switch>
-  <Route exact path = "/">
-    <Main/>
-  </Route>
-  <div className="page">
-    {/*<Header/>*/}
-  </div>
-</Switch>
-
+    <div className="page">
+      <Switch>
+        <Route exact path = "/">
+          <Main/>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
