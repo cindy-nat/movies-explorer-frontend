595ce316cc1558f4ba7377905bbcd88a937b7803
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-24 15:21:51 +0300

    added route for signup in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Main from "../Main/Main";
+import Register from "../Register/Register";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <Switch>
         <Route exact path = "/">
           <Main/>
+        </Route>
+        <Route path = "/signup">
+          <Register/>
         </Route>
       </Switch>
     </div>
