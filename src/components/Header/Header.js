7f04e8f84bb4b9bcd2fa30cde2895912ce57eb8e
Natalia Popova <cindy.n-popova@yandex.ru>
2021-02-25 13:42:06 +0300

    done header layout for authorized pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import './Header.css';
-import React from "react";
+import React from 'react';
 import logo from '../../images/logo.png';
 import {Link, Route, Switch} from "react-router-dom";
-import Logo from "../../images/logo.png";
+import Logo from '../../images/logo.png';
+import personIcon from '../../images/profile-icon.png';
+import Navigation from "../Navigation/Navigation";
 
 function Header({ headerText }) {
   return (
@@ -10,7 +12,7 @@ function Header({ headerText }) {
       <Route exact path = "/">
         <header className="header header_page_main">
           <div className="header__container">
-            <img className="header__logo header__logo_page_main" src={Logo} alt='логотип'/>
+            <Link to='/'><img className="header__logo header__logo_page_main" src={Logo} alt='логотип'/></Link>
             <nav className="header__buttons">
               <Link to='/signup' className="header__button-register">Регистрация</Link>
               <Link to='/signin' className="header__button-signin">Войти</Link>
@@ -20,11 +22,26 @@ function Header({ headerText }) {
       </Route>
 
       <Route path={['/signup', '/signin']}>
-        <div className="header header_page_signup">
-        <Link to='/'><img className="header__logo header__logo_page_signup" src = {logo} alt = "логотип"/></Link>
+        <header className='header header_page_notAuthorized'>
+        <Link to='/'><img className='header__logo header__logo_page_notAuthorized' src = {logo} alt = "логотип"/></Link>
         <h2 className="header__title">{headerText}</h2>
-      </div>
+        </header>
       </Route>
+
+      <Route path={['/movies', '/saved-movies', '/profile']}>
+        <header className='header header_page_authorized'>
+          <Link to='/'><img className='header__logo header__logo_page_main' src = {logo} alt = "логотип"/></Link>
+          <div className='header__menu-container'>
+            <Navigation/>
+            <Link to='./profile' className='header__link'>Аккаунт
+              <span className='header__icon-container'>
+                <img className='header__person-icon' src={personIcon}/>
+              </span>
+            </Link>
+          </div>
+        </header>
+      </Route>
+
     </Switch>
   );
 }
