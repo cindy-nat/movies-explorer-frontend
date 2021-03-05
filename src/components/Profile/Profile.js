3a67131078d372f001e644ec410d9bc6288d7ce8
Natalia Popova <cindy.n-popova@yandex.ru>
2021-03-05 12:20:00 +0300

    added currentUserContext to Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Profile.css';
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
 
-function Profile({ name, handleSignOut, email }) {
+
+function Profile({ handleSignOut }) {
+
+  const currentUser = React.useContext(CurrentUserContext);
+
 
   return (
       <section className='profile'>
-        <h2 className='profile__title'>Привет, {name}!</h2>
+        <h2 className='profile__title'>Привет, {currentUser.name}!</h2>
         <form className='profile__form' name='edit-form'></form>
         <label className='profile__form-label'>Имя
-          <input className='profile__form-input' type='text' value={name}></input>
+          <input className='profile__form-input' type='text' value={currentUser.name}></input>
         </label>
         <label className='profile__form-label'>Почта
-          <input className='profile__form-input' type='email' value={email}></input>
+          <input className='profile__form-input' type='email' value={currentUser.email}></input>
         </label>
         <button type='submit' className='profile__submit-button'>Редактировать</button>
         <button className='profile__exit-button' onClick={handleSignOut}>Выйти из аккаунта</button>
