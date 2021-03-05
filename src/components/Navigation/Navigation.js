e538d86ae1f64e62b915c272c79ca171623488f9
Natalia Popova <cindy.n-popova@yandex.ru>
2021-03-05 12:03:53 +0300

    changed Links to Navlink in Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navigation.css';
-import {Link, Route} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 
 function Navigation({ isMenuOpen }) {
@@ -9,32 +9,18 @@ function Navigation({ isMenuOpen }) {
     <nav className='pages-navigation'>
       <ul className='pages-navigation__list'>
         {isMenuOpen &&<li className='pages-navigation__list-item'>
-          <Link className={ `pages-navigation__link` } to='/'>
-            Главная</Link>
+          <NavLink className='pages-navigation__link' activeClassName='pages-navigation__link_active' exact to='/'>
+            Главная</NavLink>
         </li> }
-        <Route path={'/movies'}>
         <li className='pages-navigation__list-item'>
-          <Link className={`pages-navigation__link pages-navigation__link_active` } to='/movies'>
-            Фильмы</Link>
+          <NavLink className='pages-navigation__link' activeClassName='pages-navigation__link_active' to='/movies'>
+            Фильмы</NavLink>
         </li>
         <li className='pages-navigation__list-item'>
-          <Link className={`pages-navigation__link` } to='/saved-movies'>
-            Сохраненные фильмы</Link>
+          <NavLink className='pages-navigation__link' activeClassName='pages-navigation__link_active' to='/saved-movies'>
+            Сохраненные фильмы</NavLink>
         </li>
-        </Route>
-
-        <Route path={'/saved-movies'}>
-        <li className='pages-navigation__list-item'>
-          <Link className={`pages-navigation__link`} to='/movies'>
-            Фильмы</Link>
-        </li>
-        <li className='pages-navigation__list-item'>
-          <Link className={`pages-navigation__link pages-navigation__link_active` } to='/saved-movies'>
-            Сохраненные фильмы</Link>
-        </li>
-        </Route>
       </ul>
-
     </nav>
   );
 }
